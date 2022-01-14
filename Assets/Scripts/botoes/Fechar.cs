b9f92d034d104c108b653ecff5f571a094cbd688
Ricardo Gomes <sleiph@gmail.com>
2022-01-13 22:15:28 -0300

    escrevendo os primeiros scripts

---
+++ Assets/Scripts/botoes/Fechar.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fechar : MonoBehaviour
+{
+    public void FecharJogo() {
+        Application.Quit();
+    }
+}
